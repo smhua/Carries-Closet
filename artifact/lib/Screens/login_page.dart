a0c323aee3ae33340dc46e85694b922944fe2c2e
Steven Hua <stevenhua@gatech.edu>
2023-02-03 12:53:53 -0500

    test branch additions

--- artifact/lib/Screens/login_page.dart
+++ artifact/lib/Screens/login_page.dart
@@ -2,7 +2,14 @@ import 'package:artifact/Screens/open_page.dart';
 import "package:flutter/material.dart";
 import "package:artifact/main.dart";
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
