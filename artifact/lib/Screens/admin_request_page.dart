0dfd6bcad8f4f27360ebe3009cb8c88cd5edcccb
cjeong41 <cjeong41@gatech.edu>
2023-03-06 12:43:46 -0500

    json decode

--- artifact/lib/Screens/admin_request_page.dart
+++ artifact/lib/Screens/admin_request_page.dart
@@ -1,10 +1,16 @@
+import 'dart:convert';
+
 import 'package:artifact/Screens/open_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
 import "package:artifact/main.dart";
 import 'package:artifact/Screens/hygiene_confirmation_page.dart';
 import "package:artifact/home_page.dart";
 import "package:artifact/main.dart";
+
+import 'package:http/http.dart' as http;
+// import json;
 
 class AdminRequestPage extends StatefulWidget {
   const AdminRequestPage({super.key});
@@ -16,38 +22,88 @@ class AdminRequestPage extends StatefulWidget {
 }
 
 class _AdminRequestPageState extends State<AdminRequestPage> {
+  CollectionReference referenceRequests =
+      FirebaseFirestore.instance.collection('requests');
+
+  late Stream<QuerySnapshot> streamRequests;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    streamRequests = referenceRequests.snapshots();
+  }
+
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
+    // referenceRequests.snapshots();
+
+    // double width = MediaQuery.of(context).size.width;
+    // double height = MediaQuery.of(context).size.height;
 
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-            body: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(children: [
-                  SizedBox(height: height * 1.0 / 18.0),
-                  Stack(alignment: Alignment.topLeft, children: [
-                    Align(
-                        alignment: Alignment.topLeft,
-                        child: IconButton(
-                            iconSize: width * 1.0 / 18.0,
-                            onPressed: () {
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: ((context) {
-                                return HomePage();
-                              })));
-                            },
-                            icon: const Icon(Icons.arrow_back))),
-                    Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Image.asset("assets/dsdf1.png",
-                            height: height * 1.0 / 6.75,
-                            width: height * 1.0 / 6.75,
-                            alignment: Alignment.center))
-                  ]),
-                  RequestCardWidget(),
-                ]))));
+            body: StreamBuilder<QuerySnapshot>(
+          stream: streamRequests,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              viewRequest();
+            } else if (snapshot.hasError) {
+              return Center(child: Text(snapshot.error.toString()));
+            } else if (snapshot.connectionState == ConnectionState.active) {
+              QuerySnapshot querySnapshot = snapshot.data;
+            }
+            return Center(child: CircularProgressIndicator());
+          },
+        )));
+
+    // return MaterialApp(
+    //     debugShowCheckedModeBanner: false,
+    //     home: Scaffold(
+    //         body: SingleChildScrollView(
+    //             scrollDirection: Axis.vertical,
+    //             child: Column(children: [
+    //               SizedBox(height: height * 1.0 / 18.0),
+    //               Stack(alignment: Alignment.topLeft, children: [
+    //                 Align(
+    //                     alignment: Alignment.topLeft,
+    //                     child: IconButton(
+    //                         iconSize: width * 1.0 / 18.0,
+    //                         onPressed: () {
+    //                           Navigator.push(context,
+    //                               MaterialPageRoute(builder: ((context) {
+    //                             return HomePage();
+    //                           })));
+    //                         },
+    //                         icon: const Icon(Icons.arrow_back))),
+    //                 Align(
+    //                     alignment: Alignment.bottomCenter,
+    //                     child: Image.asset("assets/dsdf1.png",
+    //                         height: height * 1.0 / 6.75,
+    //                         width: height * 1.0 / 6.75,
+    //                         alignment: Alignment.center))
+    //               ]),
+    //               RequestCardWidget(),
+    //             ]))));
+  }
+
+  Future viewRequest() async {
+    print('view hygiene called');
+    bool isIOS = Theme.of(context).platform == TargetPlatform.iOS;
+    var url = isIOS
+        ? Uri.parse('http://127.0.0.1:8080/requests/hygiene/list')
+        : Uri.parse('http://10.0.2.2:8080/requests/hygiene/list');
+
+    var response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    var parsed = json.decode(response.body);
+    print(parsed[1]);
+    print(parsed[1]['address']);
+    // print(parsed[0].)
+    // for (var i in parsed) {
+    //   print(i);
+    // }
   }
 }
 
